e5235073a6ae4215c776997d03c7d3ca8d0f1bda
shoirata <shoira.shakirovna@gmail.com>
2022-01-30 20:34:38 +0400

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Button, Menu, Typography, Avatar  } from 'antd'
 import {Link} from 'react-router-dom'
-import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
+import {HomeOutlined, BulbOutlined, FundOutlined, MenuOutlined} from '@ant-design/icons'
 import icon from '../images/cryptocurrency.png'
 
 function Navbar() {
@@ -39,15 +39,10 @@ function Navbar() {
       <Menu.Item icon={<FundOutlined/>}> 
         <Link to='cryptocurrencies'>Cryptocurrencies</Link>
       </Menu.Item>
-      <Menu.Item icon={<MoneyCollectOutlined/>}> 
-        <Link to='/exchanges'>Exchanges</Link>
-      </Menu.Item>
       <Menu.Item icon={<BulbOutlined/>}> 
         <Link to='/news'>News</Link>
       </Menu.Item>
     </Menu>
-    
-    
     }
   </div>;
 }
